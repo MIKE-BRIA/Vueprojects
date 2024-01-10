3c0473bdd1bba04ea8dbf33b9e8510d5b79d2b02
Brian <brianmichaeladero@gmail.com>
2024-01-10 04:19:46 +0300

    expense-tracker

---
+++ expense-tracker/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <body>
+    <Header />
+    <div class="container">
+      <Balance />
+      <IncomeExpense />
+      <TransactionList />
+      <AddTransaction />
+    </div>
+  </body>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpense from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpense,
+    TransactionList,
+    AddTransaction,
+  },
+};
+</script>
+
+<style></style>
---
+++ expense-tracker/src/components/Balance.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h4>Your Balance</h4>
+    <h1 id="balance">$0.00</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
