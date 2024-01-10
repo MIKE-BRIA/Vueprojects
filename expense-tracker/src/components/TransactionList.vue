931f590e8de9d8b9ec305e962537bda189410e33
Brian <brianmichaeladero@gmail.com>
2024-01-10 05:33:37 +0300

    child-Transaction page

--- expense-tracker/src/components/TransactionList.vue
+++ expense-tracker/src/components/TransactionList.vue
@@ -2,18 +2,28 @@
   <div>
     <h3>History</h3>
     <ul id="list" class="list">
-      <!-- <li class="minus">
-        Cash <span>-$400</span><button class="delete-btn">x</button>
+      <li
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'"
+      >
+        {{ transaction.text }} <span>${{ transaction.amount }}</span
+        ><button class="delete-btn">x</button>
       </li>
-      <li class="plus">
-        Paycheck <span>$400</span><button class="delete-btn">x</button>
-      </li> -->
     </ul>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+//adding a prop
+import { defineProps } from "vue";
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
 </script>
 
 <style></style>
