a9a3a9bd9ab6e3cbccab0e89c00ac1715febf5ca
Brian <brianmichaeladero@gmail.com>
2024-01-10 05:30:19 +0300

    parent-serve point

--- expense-tracker/src/App.vue
+++ expense-tracker/src/App.vue
@@ -2,30 +2,55 @@
   <body>
     <Header />
     <div class="container">
-      <Balance />
-      <IncomeExpense />
-      <TransactionList />
+      <Balance :total="total" />
+      <IncomeExpense :income="income" :expenses="expenses" />
+      <TransactionList :transactions="transactions" />
       <AddTransaction />
     </div>
   </body>
 </template>
 
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpense from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
+import { ref, computed } from "vue";
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpense,
-    TransactionList,
-    AddTransaction,
-  },
-};
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 2, text: "Car", amount: 2019.99 },
+  { id: 3, text: "Paycheck", amount: 4119.99 },
+  { id: 4, text: "food", amount: -100.99 },
+]);
+
+//Get Total Amount
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+//Get income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+//Get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
 </script>
 
 <style></style>
