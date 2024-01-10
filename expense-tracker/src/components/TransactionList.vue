8f75d9aa6110bb87235219e3d93808fe4ee1fd73
Brian <brianmichaeladero@gmail.com>
2024-01-10 07:43:10 +0300

    child-Transactionlist page

--- expense-tracker/src/components/TransactionList.vue
+++ expense-tracker/src/components/TransactionList.vue
@@ -8,7 +8,9 @@
         :class="transaction.amount < 0 ? 'minus' : 'plus'"
       >
         {{ transaction.text }} <span>${{ transaction.amount }}</span
-        ><button class="delete-btn">x</button>
+        ><button class="delete-btn" @click="deleteTransaction(transaction.id)">
+          x
+        </button>
       </li>
     </ul>
   </div>
@@ -18,12 +20,18 @@
 //adding a prop
 import { defineProps } from "vue";
 
+const emit = defineEmits(["DeleteTransaction"]);
+
 const props = defineProps({
   transactions: {
     type: Array,
     required: true,
   },
 });
+
+const deleteTransaction = (id) => {
+  emit("DeleteTransaction", id);
+};
 </script>
 
 <style></style>
