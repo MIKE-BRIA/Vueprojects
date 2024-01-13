f8156b23e59e261a6fa562eb642d03f8fc86710c
Brian <brianmichaeladero@gmail.com>
2024-01-13 14:42:14 +0300

    Income and expense display

--- moneyspend/src/components/IncomeExp.vue
+++ moneyspend/src/components/IncomeExp.vue
@@ -2,18 +2,29 @@
   <div class="incomeexp">
     <div>
       <h3>Income</h3>
-      <p class="moneyplus">+9000</p>
+      <p class="moneyplus">+{{ income }}</p>
     </div>
 
     <div>
       <h3>Expence</h3>
-      <p class="moneyminus">-5001</p>
+      <p class="moneyminus">${{ expense }}</p>
     </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  income: {
+    type: Number,
+    required: true,
+  },
+  expense: {
+    type: Number,
+    required: true,
+  },
+});
 </script>
 
 <style></style>
