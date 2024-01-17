b37d0d7d9e999930b73d208a6639ee05d1fb58ca
Brian <brianmichaeladero@gmail.com>
2024-01-17 10:35:51 +0300

    Homepage

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,6 +2,13 @@
   <main class="container">
     <h1>Our cars</h1>
 
+    <select @change="handlechange" v-model="make">
+      <option value="All">All</option>
+      <option value="Chevrolet">Chevy</option>
+      <option value="Porsche">Porsche</option>
+      <option value="Buick">Buick</option>
+      <option value="Audi">Audi</option>
+    </select>
     <div class="cards">
       <div
         class="card"
@@ -18,12 +25,35 @@
 
 <script setup>
 import carsData from "../data/data.json";
-import { ref } from "vue";
-import { useRouter } from "vue-router"; //adding functionality to our path
+import { ref, watch, onMounted } from "vue";
+import { useRouter, useRoute } from "vue-router"; //adding functionality to our path
+
+const make = ref("");
 
 const router = useRouter();
+const route = useRoute();
 
 const cars = ref(carsData);
+
+onMounted(() => {
+  make.value = route.query.make;
+  console.log(route.query);
+});
+
+//filtering the cars by their make
+watch(make, () => {
+  if (make.value) {
+    if (make.value == "All") cars.value = carsData;
+    else {
+      cars.value = carsData.filter((car) => car.make == make.value);
+    }
+  }
+});
+
+//saving query parameters to our path
+const handlechange = () => {
+  router.push({ query: { make: make.value } });
+};
 </script>
 
 <style></style>
