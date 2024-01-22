0b3e5fa7c7b65e320834ec37b1f0ff268429a51d
Brian <brianmichaeladero@gmail.com>
2024-01-22 23:25:54 +0300

    homeview

--- shop/src/views/Home.vue
+++ shop/src/views/Home.vue
@@ -18,12 +18,29 @@
 
 <script setup>
 import { ref } from "vue";
+
 import { useRouter } from "vue-router";
 import data from "../data/data.json";
 
 const router = useRouter();
 
 const Products = ref(data);
+
+// const searchQuery = ref("");
+
+// const filteredProducts = computed(() => {
+//   return products.value.filter((product) =>
+//     product.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+//   );
+// });
+
+// const filterProducts = (query) => {
+//   searchQuery.value = query;
+// };
+
+// const navigateToProduct = (id) => {
+//   router.push(`/product/${id}`);
+// };
 </script>
 
 <style></style>
