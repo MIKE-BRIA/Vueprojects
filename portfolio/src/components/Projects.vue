3d4e809b4d1ca167e1fa529dc2f3e98b51b7594e
Brian Michael <brianmichaeladero@gmail.com>
2024-02-14 13:32:01 +0300

    Projects section

---
+++ portfolio/src/components/Projects.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <div class="head">
+      <div>
+        <h1 class="top">Projects i have worked on</h1>
+      </div>
+      <div class="line"></div>
+      <p>These are all the projects that have worked on</p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+main {
+  margin-top: 9rem;
+}
+</style>
