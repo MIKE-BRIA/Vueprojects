7903b95e2ac51266b6d93a5573e2bed2660fc364
Brian Michael <brianmichaeladero@gmail.com>
2024-02-14 13:32:35 +0300

    footer

---
+++ portfolio/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <p>Designed & Built by Brian Michael</p>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+main {
+  margin-top: 6rem;
+}
+
+p {
+  font-style: italic;
+  font-size: 1.2rem;
+  letter-spacing: 1px;
+}
+</style>
