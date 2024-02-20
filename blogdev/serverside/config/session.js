00efa85ef95fcbedd36462a8add0357338a62045
Brian Michael <brianmichaeladero@gmail.com>
2024-02-20 14:28:55 +0300

    session config

---
+++ blogdev/serverside/config/session.js
@@ -0,0 +1,31 @@
+const mongodbStore = require("connect-mongodb-session");
+const session = require("express-session");
+
+//*session store
+function sessionStore() {
+  const MongoDBStore = mongodbStore(session);
+
+  const store = new MongoDBStore({
+    uri: "mongodb://127.0.0.1:27017",
+    databaseName: "devblog",
+    collection: "sessions",
+  });
+
+  return store;
+}
+
+//* session configuration
+
+function sessionConfig() {
+  return {
+    secret: "hardpasswords",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore(),
+    cookie: {
+      maxAge: 3600 * 24 * 60 * 60 * 1000,
+    },
+  };
+}
+
+module.exports = sessionConfig;
