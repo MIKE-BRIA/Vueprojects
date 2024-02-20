ef54d2ec8b585285b78ae6d752d5364649ab6ff1
Brian Michael <brianmichaeladero@gmail.com>
2024-02-20 11:36:21 +0300

    authentication routes

--- blogdev/serverside/routes/auth.routes.js
+++ blogdev/serverside/routes/auth.routes.js
@@ -4,13 +4,10 @@ const authcontroller = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-//router for signup
-router.post("/login", authcontroller.signup);
-//get
-router.post("/signup", (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! thank you for joining our service`,
-  });
-});
+//*router for signup
+router.post("/signup", authcontroller.signup);
+
+//*login route
+router.post("/login", authcontroller.login);
 
 module.exports = router;
