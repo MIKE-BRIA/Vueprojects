43deedf9d8f51204efdaa19e61c0434b67311f15
Brian Michael <brianmichaeladero@gmail.com>
2024-02-21 14:25:15 +0300

    auth routes

--- blogdev/serverside/routes/auth.routes.js
+++ blogdev/serverside/routes/auth.routes.js
@@ -10,4 +10,7 @@ router.post("/signup", authcontroller.signup);
 //*login route
 router.post("/login", authcontroller.login);
 
+//*check if user is authenticated
+router.get("/checkauth", authcontroller.checkauth);
+
 module.exports = router;
